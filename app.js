df7c0f505697ebd77cf699aac08ef1d22dbb44b0
Colin <colin1124x@gmail.com>
2015-08-03 16:27:23 +0800

    try to keep conn alive

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var server = ws.createServer(function (conn) {
 setInterval(function(){
     var alive_conn = [];
     for (var id in conn_map) {
-        conn_map[id].sendBinary && conn_map[id].sendBinary();
+        conn_map[id].sendBinary && conn_map[id].sendBinary(new Buffer(0));
         alive_conn.push(id);
     }
 
