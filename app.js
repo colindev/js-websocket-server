5d38d5a79ae3578b945e59a2c330eb3e6a92c8ab
Colin <colin1124x@gmail.com>
2015-08-03 11:56:17 +0800

    change app.js

--- app.js
+++ app.js
@@ -2,19 +2,30 @@
 
 var port = parseInt(process.argv[2], 10);
 var ws = require("nodejs-websocket");
+var qs = require('querystring');
 
 if (isNaN(port)) throw new Error('port can\'t be '+port);
 
+function log_time(){
+    return (new Date).toISOString();
+}
 // Scream server example: "hi" -> "HI!!!"
 var server = ws.createServer(function (conn) {
-    console.log("New connection");
+
+    var data = qs.parse(conn.path.replace(/^[^?]*\?/, '')),
+        key = data.key;
+
+    console.log(log_time()+' Connection: ', key);
+
     conn.on("text", function (str) {
-        console.log("Received "+str);
-        conn.sendText(str.toUpperCase()+"!!!");
-        conn.sendText('news: hello channel\n....');
-        conn.sendText('news: {"hello":"obj"}');
+
+        var ping_in = (new Date).getTime();
+
+        console.log(log_time()+' Received: '+key+' [ '+str+' ] '+ping_in);
+        conn.sendText('pong:in:'+ping_in+':out:'+(new Date).getTime());
+
     });
     conn.on("close", function (code, reason) {
-        console.log("Connection closed")
+        console.log(log_time()+' Connection closed: '+key);
     })
 }).listen(port);
